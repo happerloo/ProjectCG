0f73562b4f3469fd9a6d38459c12b882649bb948
Hanze298208 <50943917+Hanze298208@users.noreply.github.com>
2019-06-03 13:58:21 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,52 @@
+<?php include 'head.php';?>     
+
+<nav class="navbar navbar-expand navbar-dark fixed-top navbar-custom menu">
+          <a class="navbar-brand" href="#">
+    <img class="logo" src="/campus/logo.png" alt="Logo">
+  </a>
+     <ul class="navbar-nav ml-auto d-none d-lg-block">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kaart</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Projecten</a>
+      </li> 
+           <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+         
+                    <li class="nav-item">
+              <button class="btn-search" type="button" onclick="myFunction()">
+        <i class="fa fa-search"></i>
+      </button>
+      </li>
+    </ul>
+           <ul class="navbar-nav ml-auto d-lg-none">
+          
+                   <li class="nav-item">
+                         <button class="btn-search" type="button" onclick="myFunction()">
+        <i class="fa fa-search"></i>
+      </button>
+      
+
+      </li>
+          </ul>
+        
+       </nav>
+       
+                <div class="search" id="search">
+                    
+ <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search this blog">
+    <div class="input-group-append">
+      <button class="btn btn-secondary" type="button">
+        <i class="fa fa-search"></i>
+      </button>
+    </div>
+  </div>
+</div>
+
+<body>
