bf9e3971c95aede2ecd724b02a501368325da4be
Hanze298208 <50943917+Hanze298208@users.noreply.github.com>
2019-06-03 13:52:12 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,78 @@
+
+
+<footer>
+    
+    <nav class="icon-bar menu  d-sm-none">
+  <a href="#"><i class="fa fa-info"></i><p>Informatie</p></a>
+<a href="#"><i class="fa fa-map"></i><p>Kaart</p></a>
+           <a href="#"><i class="fa fa-building"></i><p>Projecten</p></a>
+ <a href="#"><i class="fa fa-comments"></i><p>Contact</p></a>
+
+</nav>
+        
+        
+        
+        <div class="container">
+        
+        <div class="row">
+            
+       <div class="col-sm-12 col-md-6">
+             
+             <p><strong>Schrijf je in voor de niewsbrief.</strong></p>
+             <p>Deze Campus Groningen nieuwsbrief verschijnt ieder kwartaal.</p>
+             
+            </div>
+            
+            <div class="col-sm-12 col-md-6">
+                
+                <ul>
+                        <li>
+                           
+                                <input type="text" class="" placeholder="Email ">
+                                <button class="btn btn-success" type="button"> Lorem ipsum <i class="fa fa-long-arrow-right"> </i> </button>
+                         
+                        </li>
+                    </ul>
+            </div>
+       
+            <div class="col-12">
+                    <ul class="social">
+                        <li> <a href="#"> <i class="fa fa-facebook">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-pinterest">   </i> </a> </li>
+                        <li> <a href="#"> <i class="fa fa-youtube">   </i> </a> </li>
+                    </ul>
+                
+            </div>
+            
+            </div>
+        
+        </div>
+        
+        
+       
+       </footer>
+
+      
+    <!-- Optional JavaScript -->
+      
+      <script> 
+      
+          function myFunction() {
+  var x = document.getElementById("search");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+      
+      </script>
+      
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html> 
