f3ecf98542740b3154e835aa05b4d59e49c94047
Hanze298208 <50943917+Hanze298208@users.noreply.github.com>
2019-06-03 14:03:01 +0200

    Add files via upload

---
+++ projectdetails.php
@@ -0,0 +1,55 @@
+<?php include 'head.php';?>
+<?php include 'header.php';?>
+
+    <!--Zet hieronder een foto van het gebouw waar aan wordt gewerkt-->
+    <img class="img-fluid" src="img/vanolsttoren.jpg" alt="Een foto van de van Olsttoren">
+    
+    <!--De volgende div bevat de titel van het project-->
+    <div class="bg-dark">
+        <div class="d-flex container">
+            <!--Voeg hier de titel toe van het project-->
+            <h1 class="font-weight-bold text-white">
+               Verbouwing aan de van Olsttoren
+            </h1>
+        </div>
+    </div>
+    
+    <!--Voeg hieronder alle informatie toe van het project-->
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Projectinformatie</h2>
+                <p>
+                   De Hanzehogeschool verbouwt vanaf 2018 het complex van de Van Olst Toren (ZP7). Het doel is de reactie van een toegankelijk campusgebouw, en een levendig middelpunt voor studenten, medewerkers én bezoekers.
+                </p>
+                <h2>Hoofdstraat</h2>
+                <p>
+                   In de nieuwe opzet van het complex wordt er een hoofdstraat gemaakt die het verbindend element zal vormen binnen het complex. Het Atrium vormt dan het kruis- en ontmoetingspunt. Hiervoor zullen enkele gebouwdelen worden gesloopt. Er komt duurzame nieuwbouw voor in de plaats, die wordt geïntegreerd met de huidige gebouwdelen.
+                </p>
+            <h2>Conferentieruimte</h2>
+                <p>
+                   In de nieuwbouw zal o.a. plaats zijn voor een grote conferentieruimte.
+                </p>
+                <h2>Binnentuinen</h2>
+                <p>
+                    Ook worden er binnentuinen gecreeërd, met ontspannen verblijfsruimtes, die ook zorgen voor veel licht in de gebouwdelen.
+                </p>
+                <h2>Tijdelijke huisvesting</h2>
+                <p>
+                    Om studenten, medewerkers en bezoekers zo weinig mogelijk overlast te bezorgen, is er tijdelijke huisvesting gerealiseerd bij ZP7 in mei 2018: het U-gebouw.
+                </p>
+        </div>
+        <!--de volgende column bevat afbeeldingen van de bouw van het project. Laat deze hele column weg als er geen afbeeldingen zijn-->
+        <div class="col">
+          <h2>Foto's van de bouw</h2>
+                <img src="img/vanolsttoren.jpg" alt="Beschrijving foto 1" class="img-thumbnail">
+                <img src="img/vanolsttoren.jpg" alt="Beschrijving foto 1" class="img-thumbnail">
+                <img src="img/vanolsttoren.jpg" alt="Beschrijving foto 1" class="img-thumbnail">
+       </div>
+      </div>
+      
+<?php include 'footer.php';?>
+
+
+
+    
