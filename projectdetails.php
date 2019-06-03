4909c19e7e4f2a29f251a77dae88490efb45bafe
QueenJinx <51085001+QueenJinx@users.noreply.github.com>
2019-06-03 14:00:54 +0200

    Add files via upload

---
+++ projectdetails.php
@@ -0,0 +1,22 @@
+/* Padding kan weggehaald worden na het toevoegen van menu */
+body {
+  padding-top: 2rem;
+
+font-family: 'Montserrat', sans-serif;
+font-size: 12px;
+
+}
+
+h1, h2 {
+font-weight: bold;
+  color: #3B3B3C;
+}
+
+p {
+  color: #3B3B3C;
+}
+
+/* Dit is de lange header, verwijder of plaats hier een afbeelding in */
+.jumbotron {
+  background-color: #add8e5;
+}
