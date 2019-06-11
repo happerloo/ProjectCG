8edfbbe5d2d3449f1d3fddb866a976deb2d9b6f6
Hanze298208 <50943917+Hanze298208@users.noreply.github.com>
2019-06-11 14:36:00 +0200

    Add files via upload

---
+++ projecten.php
@@ -0,0 +1,86 @@
+<?php include 'head.php';?>
+<?php include 'header.php';?>
+
+<div class="content">
+    <!--Zet hieronder een foto van het gebouw waar aan wordt gewerkt-->
+    <img class="img-fluid" src="img/vanOlst_zuidgevel_omslag.jpg" alt="Een sfeerimpressie van de zuidgevel van de van Olst Toren">
+    
+    <!--De volgende div bevat de titel van het project-->
+    <div class="bg-dark">
+        <div class="d-flex container">
+            <!--Voeg hier de titel toe van het project-->
+            <h1 class="font-weight-bold text-white">
+               Projecten
+            </h1>
+        </div>
+    </div>
+    
+    <!--Voeg hieronder alle informatie toe van het project-->
+    <div class="container">
+      <div class="row border mt-2">
+        <div class="col-sm-12 col-md-6">
+               <img src="img/vanOlst_zuidgevel_omslag.jpg" alt="Sfeerimpressie nieuw atrium" class="img-fluid">
+            
+          </div>
+          
+            <div class="col-sm-12 col-md-6">
+                <h2>Zernikelaan</h2>
+                <p>
+                   In de nieuwe opzet van het complex wordt er een hoofdstraat gemaakt die het verbindend element zal vormen binnen het complex. Het Atrium vormt dan het kruis- en ontmoetingspunt. Hiervoor zullen enkele gebouwdelen worden gesloopt. Er komt duurzame nieuwbouw voor in de plaats, die wordt geïntegreerd met de huidige gebouwdelen.
+                </p>
+              
+          </div>
+    
+        </div>
+        
+         <div class="row border mt-2">
+        <div class="col-sm-12 col-md-6">
+               <img src="img/vanOlst_zuidgevel_omslag.jpg" alt="Sfeerimpressie nieuw atrium" class="img-fluid">
+            
+          </div>
+          
+            <div class="col-sm-12 col-md-6">
+                <h2>Alleta Jacobshal</h2>
+                <p>
+                   In de nieuwe opzet van het complex wordt er een hoofdstraat gemaakt die het verbindend element zal vormen binnen het complex. Het Atrium vormt dan het kruis- en ontmoetingspunt. Hiervoor zullen enkele gebouwdelen worden gesloopt. Er komt duurzame nieuwbouw voor in de plaats, die wordt geïntegreerd met de huidige gebouwdelen.
+                </p>
+              
+          </div>
+    
+        </div>
+         <div class="row border mt-2">
+        <div class="col-sm-12 col-md-6">
+               <img src="img/vanOlst_zuidgevel_omslag.jpg" alt="Sfeerimpressie nieuw atrium" class="img-fluid">
+            
+          </div>
+          
+            <div class="col-sm-12 col-md-6">
+                <h2>Van Olst Toren</h2>
+                <p>
+                   In de nieuwe opzet van het complex wordt er een hoofdstraat gemaakt die het verbindend element zal vormen binnen het complex. Het Atrium vormt dan het kruis- en ontmoetingspunt. Hiervoor zullen enkele gebouwdelen worden gesloopt. Er komt duurzame nieuwbouw voor in de plaats, die wordt geïntegreerd met de huidige gebouwdelen.
+                </p>
+              
+          </div>
+    
+        </div>
+       <div class="row border mt-2">
+        <div class="col-sm-12 col-md-6">
+               <img src="img/vanOlst_zuidgevel_omslag.jpg" alt="Sfeerimpressie nieuw atrium" class="img-fluid">
+            
+          </div>
+          
+            <div class="col-sm-12 col-md-6">
+                <h2>Feringabuilding</h2>
+                <p>
+                   In de nieuwe opzet van het complex wordt er een hoofdstraat gemaakt die het verbindend element zal vormen binnen het complex. Het Atrium vormt dan het kruis- en ontmoetingspunt. Hiervoor zullen enkele gebouwdelen worden gesloopt. Er komt duurzame nieuwbouw voor in de plaats, die wordt geïntegreerd met de huidige gebouwdelen.
+                </p>
+              
+          </div>
+    
+        </div>
+
+    
+      </div>
+</div>
+      
+<?php include 'footer.php';?>
