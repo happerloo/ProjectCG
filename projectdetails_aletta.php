b7a4a7447dffe6e9230af1c5ce87c3c35c4bbe5b
QueenJinx <51085001+QueenJinx@users.noreply.github.com>
2019-06-11 15:23:16 +0200

    Add files via upload

---
+++ projectdetails_aletta.php
@@ -0,0 +1,54 @@
+<?php include 'head.php';?>
+<?php include 'header.php';?>
+
+<div class="content">
+    <!--Zet hieronder een foto van het gebouw waar aan wordt gewerkt-->
+    <img class="img-fluid" src="img/aletta_omslag.png" alt="Een sfeerimpressie van de nieuwe Aletta Jacobshal">
+    
+    <!--De volgende div bevat de titel van het project-->
+    <div class="bg-dark">
+        <div class="d-flex container">
+            <!--Voeg hier de titel toe van het project-->
+            <h1 class="font-weight-bold text-white">
+               Uitbreiding Aletta Jacobshal
+            </h1>
+        </div>
+    </div>
+    
+    <!--Voeg hieronder alle informatie toe van het project-->
+    <div class="container project_margin">
+      <div class="row">
+        <div class="col">
+          <h2>Projectinformatie</h2>
+                <p>
+                   De wens van faculteiten en docenten van de universiteit is helder: meer tentamens (digitaal) kunnen afnemen tijdens de tentamenperiode. De bestaande Aletta Jacobshal op de Zernike Campus heeft daarvoor op dit moment te weinig capaciteit. Daarom heeft de Rijksuniversiteit Groningen (RUG) samen met architectenbureau Onix NL uit Groningen een plan gemaakt voor een uitbreiding met meer college- en tentamenruimten, een extra foyer/wachtruimte, studielandschap en meer fietsenstallingen. BAM Bouw en Techniek is de aannemer die het project gaat uitvoeren.
+            </p>
+            <p>
+                    Het gebouw wordt aangesloten op een WKO-installatie (Warmte Koude Opslag) voor het duurzaam verwarmen en koelen. Binnen het project worden er bovendien 576 zonnepanelen op het dak van het complex aangebracht en krijgt de foyer een betere klimaatbeheersing. Naar verwachting start de bouw eind 2018 en zal de ingebruikname rond begin 2020 zijn.
+
+                </p>
+                <h2>Verdubbeling</h2>
+                <p>
+                   In de Aletta Jacobshal komen er twee nieuwe collegezalen bij, één zaal van 200 zitplaatsen en één zaal van 450 zitplaatsen, en een zaal met 590 (digitale) tentamenplaatsen. De uitbreiding levert zo’n 5.400 m2 extra (bruto) vloeroppervlak op, nagenoeg een verdubbeling van de huidige situatie. De nieuwbouw wordt aan de zuidoostzijde tegen de bestaande hal aangebouwd, deels in de vijver. De fietsenstalling krijgt aan de achterzijde een uitbreiding en gaat van 1.060 naar ca. 1.550 plaatsen.
+                </p>
+                <p>
+                    Daarnaast komt er aanzienlijk meer wachtruimte voor studenten door een extra foyer toe te voegen. De huidige entree zal daardoor meer als uitgang worden gebruikt. Door een beter klimaatbeheersingssysteem zal het binnenklimaat in de bestaande foyer bovendien sterk verbeteren. Ten slotte is er in het ontwerp ruimte voor studieplekken gecreëerd.
+                </p>
+            <h2>Overlastbeperking tijdens tentamens</h2>
+                <p>
+                   De grote uitdaging in dit project is uiteraard het beperken van overlast als gevolg van de bouwwerkzaamheden. Een belangrijk selectiecriterium voor de aannemer tijdens de aanbesteding was dan ook het slim plannen van bouwwerkzaamheden rondom tentamenperiodes. Meer details over de planning en maatregelen volgen later.
+                </p>
+        </div>
+        <!--de volgende column bevat afbeeldingen van de bouw van het project. Laat deze hele column weg als er geen afbeeldingen zijn-->
+        <div class="col">
+          <h2>Foto's van de bouw</h2>
+                <img src="img/aletta_binnen.jpg" alt="Sfeerimpressie Aletta Jacobshal" class="img-fluid img-thumbnail">
+                <img src="img/aletta_collegezaal.jpg" alt="Sfeerimpressie Aletta Jacobshal" class="img-fluid img-thumbnail">
+                <img src="img/aletta_overzicht.jpg" alt="Sfeerimpressie Aletta Jacobshal" class="img-fluid img-thumbnail">
+       </div>
+      </div>
+</div>
+</div>
+
+      
+<?php include 'footer.php';?>
