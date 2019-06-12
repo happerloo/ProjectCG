269604a69c21d58b9a953cb36543952288c1b4b3
melissaschriever <35446656+melissaschriever@users.noreply.github.com>
2019-06-12 12:37:09 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
-<?php include 'head.php';?>     
+<?php include 'head.php';?>
 
 
 <header>
 
   <nav class="navbar navbar-expand navbar-dark menu-top">
           <a class="navbar-brand" href="#">
-    <img class="logo" src="/campus/logo.png" alt="Logo">
+    <img class="logo" src="img/logo_campus.png" alt="Logo">
   </a>
      <ul class="navbar-nav ml-auto d-none d-lg-block">
       <li class="nav-item">
@@ -16,11 +16,11 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Projecten</a>
-      </li> 
+      </li>
            <li class="nav-item">
         <a class="nav-link" href="#">Contact</a>
       </li>
-         
+
                     <li class="nav-item">
               <button class="btn-search" type="button" onclick="myFunction()">
         <i class="fa fa-search"></i>
@@ -28,22 +28,22 @@
       </li>
     </ul>
            <ul class="navbar-nav ml-auto d-lg-none">
-          
+
                    <li class="nav-item">
                          <button class="btn-search" type="button" onclick="myFunction()">
         <i class="fa fa-search"></i>
       </button>
-      
+
 
       </li>
           </ul>
-       
-    
+
+
        </nav>
-       
-   
+
+
               <div class="search" id="search">
-                    
+
  <div class="input-group">
     <input type="text" class="form-control" placeholder="Search this blog">
     <div class="input-group-append">
@@ -53,5 +53,5 @@
     </div>
   </div>
 </div>
-    
+
     </header>
