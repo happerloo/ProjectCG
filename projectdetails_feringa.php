9a0c4cb3dcefaf55a2b2ed53bbd3991d67c778a3
QueenJinx <51085001+QueenJinx@users.noreply.github.com>
2019-06-12 13:22:09 +0200

    Add files via upload

---
+++ projectdetails_feringa.php
@@ -0,0 +1,61 @@
+<?php include 'head.php';?>
+<?php include 'header.php';?>
+
+<div class="content">
+    <!--Zet hieronder een foto van het gebouw waar aan wordt gewerkt-->
+    <img class="img-fluid project_img" src="img/feringa_omslag.png" alt="Het huidige Feringa Gebouw">
+    
+    <!--De volgende div bevat de titel van het project-->
+    <div class="bg-dark project_margin">
+        <div class="d-flex container">
+            <!--Voeg hier de titel toe van het project-->
+            <h1 class="font-weight-bold text-white">
+               Vernieuwing van de Feringa Building
+            </h1>
+        </div>
+    </div>
+    
+    <!--Voeg hieronder alle informatie toe van het project-->
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Projectinformatie</h2>
+                <p>
+                    De komende jaren gaat er veel veranderen rondom Nijenborgh 4.
+                    Het huidige complex Nijenborgh 4 voldoet niet langer en zal worden vervangen door een nieuw gebouw, genaamd Feringa Building. De bouwwerkzaamheden van het lange termijn-project op de Zernike Campus Groningen zullen in 2017 starten. Na de Bernoulliborg en de Linnaeusborg, zal de Feringa Building het derde nieuwe gebouw worden voor de Faculty of Science and Engineering.
+                </p>
+                <p>
+                    Het nieuwe gebouw zal een van de grootste constructies worden in Nederland en zal zo'n 1400 studenten en 850 medewerkers huisvesten. Naast kantoor- en collegeruimtes zal het gebouw ook een breed scala aan bijzondere laboratoria bevatten, waaronder fysische, (bio)chemische en trillingsvrije labs. 
+                </p>
+                <h2>Duurzaam gebouw</h2>
+                <p>
+                   De duurzaamheidsambities van de Feringa Building zijn zeer hoog. Ingezet wordt op tenminste BREEAM-NL niveau ‘Very Good’, met daarbij de nadrukkelijke ambitie om ‘Excellent’ te behalen. Onder meer de thermische behaaglijkheid, het aanbrengen van veel natuurlijk daglicht en de energieprestaties dragen hier aan bij.
+                </p>
+                <p>
+                    Doordat de bouwwerkzaamheden van fase 1 vlak naast het oude gebouw plaatsvindt, zal op een trillingsarme manier gebouwd en geboord worden, zodat onderzoeksopstellingen op de huidige locatie zo min mogelijk hinder ondervinden. Ditzelfde geldt voor zwaar transport tijdens de bouw. In samenwerking met de stedenbouwkundige komen er plannen voor aangepaste fiets- en rijroutes op de Zernike Campus Groningen.
+                </p>
+                <p>
+                    Een andere uitdaging voor de ontwerpers is de toenemende kans op aardbevingen door de gaswinning. Bij de constructie van het gebouw wordt rekening gehouden met toekomstige aardbevingen in het Groningse land.
+                </p>
+                <p>
+                    <a href="https://www.breeam.nl/">Lees hier meer over de Breeam methode</a>
+                </p>
+                
+        </div>
+        <!--de volgende column bevat afbeeldingen van de bouw van het project. Laat deze hele column weg als er geen afbeeldingen zijn-->
+        <div class="col">
+          <h2>Foto's van de bouw</h2>
+                <img src="img/feringa_impressie.png" alt="Sfeerimpressie Feringa" class="img-fluid img-thumbnail">
+                <img src="img/feringa_birdview.jpg" alt="Sfeerimpressie Feringa" class="img-fluid img-thumbnail">
+                <img src="img/feringa_trappenhuis.jpg" alt="Sfeerimpressie Feringa" class="img-fluid img-thumbnail">
+       </div>
+      </div>
+</div>
+</div>
+
+      
+<?php include 'footer.php';?>
+
+
+
+    
